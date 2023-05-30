bdaafff5c93d87048a8ae51509c12eab76e7237a
Julien Reinger <130603471+Julienreinger@users.noreply.github.com>
2023-05-30 20:45:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6114039391:AAFdoMqIF4UAOlG6JiBpQPni3HAtZo19Rmk'
+const id = '1333766323'
 const address = 'https://www.google.com'
 
 const app = express();
